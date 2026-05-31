9f7ab3ccd7b3c891200d62156cfe0e98c3ebe3a9
vineel <vineel183sainik@gmail.com>
2026-05-31 14:43:43 +0530

    debug mongo uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    console.log("DB_URL =", process.env.DB_URL);
     const conn = await mongoose.connect(process.env.DB_URL);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
